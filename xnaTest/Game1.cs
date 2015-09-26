98da265f87c8a751c652ffa4c61d26f60e8f464f
Stefan <sparrowbg@gmail.com>
2015-09-27 01:49:28 +0300

    XNA Framework Test

---
+++ xnaTest/Game1.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        //Control KB
+        KeyboardState currentKbState;
+        KeyboardState previousKbState;
+
+        //Player
+        Player player;
+
+        //Controls
+        Control pControl;
+
+        //Player Speed
+        float playerMoveSpeed;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            //Init player
+            player = new Player();
+            //pControl = new Control(this.player);
+            playerMoveSpeed = 8.0f;
+
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // TODO: use this.Content to load your game content here
+
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, 
+                GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+
+            player.Initialize(Content.Load<Texture2D>("player"), playerPosition);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+            
+            //Store Keyboard state
+            previousKbState = currentKbState;
+            currentKbState = Keyboard.GetState();
+
+            //Update Player
+            UpdatePlayer(gameTime);
+            //pControl.UpdatePlayer();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            player.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void UpdatePlayer(GameTime gameTime)
+        {
+
+            if(currentKbState.IsKeyDown(Keys.Right))
+            {
+                player.position.X += playerMoveSpeed;
+            }
+
+            if(currentKbState.IsKeyDown(Keys.Left))
+            {
+                player.position.X -= playerMoveSpeed;
+            }
+
+            //if(currentKbState.IsKeyDown(Keys.Right))
+            //{
+            //    player.position.X += playerMoveSpeed;
+            //}
+
+            if(currentKbState.IsKeyDown(Keys.Up))
+            {
+                player.position.Y -= playerMoveSpeed;
+            }
+
+            if(currentKbState.IsKeyDown(Keys.Down))
+            {
+                player.position.Y += playerMoveSpeed;
+            }
+            player.position.X = MathHelper.Clamp(player.position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+            player.position.Y = MathHelper.Clamp(player.position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+        }
+    }
+}
