98dec715a04412269615ffb32c9bc4d4ce767118
Supbads <snutz@mail.bg>
2015-09-28 01:55:26 +0300

    Added tanks

--- TankGame/WindowsGame1/WindowsGame1/Levels/Level.cs
+++ TankGame/WindowsGame1/WindowsGame1/Levels/Level.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace WindowsGame1.Levels
 {
-    class Level
+    abstract class Level 
     {
+        private int currentLevel;
+
+        public int CurrentLevel
+        {
+            get { return this.currentLevel; }
+            set
+            {
+                if (this.currentLevel <= 0||this.currentLevel>2)  //  IF OVERCAP => throw exception
+                {
+                    throw new ArgumentException("Level", "Reaching incorect level");
+                }
+                else
+                {
+                    this.currentLevel = value;
+                };
+            }
+        }
+
+        public Level(int level)
+        {
+            this.CurrentLevel = level;
+        }
     }
 }
---
+++ TankGame/WindowsGame1/WindowsGame1/Levels/Level_1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1.Levels
+{
+    public class Level_1 : Level   // TODO:  Level 1 outline
+    {
+        
+        
+    }
+}
